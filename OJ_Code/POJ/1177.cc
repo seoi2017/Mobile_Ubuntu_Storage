fe891eb16372d90701edcc1ca822d33877afedb6
seoi2017 <iaswork@foxmail.com>
2017-04-10 21:46:13 +0800

    all

--- OJ_Code/POJ/1177.cc
+++ OJ_Code/POJ/1177.cc
@@ -13,8 +13,9 @@
 #define nl (now->l)//左边界
 #define nr (now->r)//右边界
 #define nw (now->w)//原宽度（未离散化前）
-#define ne (now->len)//直接子区间覆盖情况：1为仅左区间，2为仅右区间，3为全覆盖，0为不覆盖
 #define nc (now->cov)//覆盖层数
+#define ncl (now->col)//最左覆盖区间点
+#define ncr (now->cor)//最右覆盖区间点
 #define ns (now->sum)//覆盖长度
 #define root (&seg[1])
 #define in(l,r,h,s) (line[++top].l=(l),line[top].r=(r),line[top].h=(h),line[top].s=(s))
@@ -23,12 +24,12 @@ const int N=10010;
 typedef unsigned long long ull_t;
 int n,top=0,pnt=0,seglen=0,x1,y1,x2,y2,map[N];
 ull_t ans=0*1LL;
-struct seg_t{int sum,cov,len,w,l,r;seg_t *lc,*rc;}seg[N<<2];
+struct seg_t{int sum,cov,col,cor,w,l,r;seg_t *lc,*rc;}seg[N<<2];
 struct line_t{int l,r,h,s;}line[N];
 inline bool cmp(line_t x,line_t y){return x.h<y.h;}
 inline int build(int l,int r){
     int temp=++pnt,m=((l+r)>>1);seg_t *now=&seg[temp];
-    nl=l,nr=r,nw=map[r]-map[l],ns=nc=ne=0;
+    nl=l,nr=r,nw=map[r]-map[l],ns=nc=ncl=ncr=0;
     if(r-l!=1)lson=&seg[build(l,m)],rson=&seg[build(m,r)];
     return temp;
 }
@@ -42,13 +43,13 @@ inline int gethash(int key,int len){
     }return -1;
 }
 inline void refresh(seg_t *now){
-
+    ns=lson->sum+rson->sum;
+    if()
 }
 inline void update(seg_t *now,int l,int r,int s){
     if(l<=nl && nr<=r){
-        nc++;
-    }
-    int m=mid;
+        nc+=s,ns=nc?nw:0,ncl=nl,ncr=nr;return;
+    }int m=mid;
     if(l<mid)update(lson,l,r,s);if(r>mid)update(rson,l,r,s);
     refresh(now);
 }
@@ -59,9 +60,10 @@ void solve(){
         x2++,y2++;in(x1,x2,y1,1),map[top]=x1,in(x1,x2,y2,-1),map[top]=x2;
     }
     sort(line+1,line+1+top,cmp),sort(map+1,map+1+top),seglen=unique(map+1,map+top)-1;
-    build(1,seglen);
+    build(1,seglen);int len=0;
     for(int i=1;i<top;i++){、
         update(root,gethash(line[i].l),gethash(line[i].r),line[i].s);
+        ans+=root->sum*abs(last-root->len);
         //TODO:Add Up Answer.
     }
     printf("%llu\n",ans);
